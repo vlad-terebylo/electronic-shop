6543ae9ce368f6576023422d65acaa5356eefa6f
Vladyslav Terebylo <terebylov@ssemi.cz>
2024-09-10 21:41:51 +0200

    Created Electronic shop project

---
+++ src/main/java/com/electronic_shop_tvo/electronicshoptvo/ElectronicShopTvoApplication.java
@@ -0,0 +1,13 @@
+package com.electronic_shop_tvo.electronicshoptvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicShopTvoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicShopTvoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/electronic_shop_tvo/electronicshoptvo/ElectronicShopTvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electronic_shop_tvo.electronicshoptvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicShopTvoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
